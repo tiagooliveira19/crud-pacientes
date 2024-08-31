6a4492a6939688d63d69ed77c37329d67edf92b4
Tiago Oliveira <tiagojf_93@hotmail.com>
2024-08-31 18:44:02 -0300

    New patient register and update patient implementations

--- web/js/functions.js
+++ web/js/functions.js
@@ -3,8 +3,148 @@ function formatesCPF(cpf) {
   return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
 }
 
+// Removes CPF mask
+function removeMask (cpf) {
+  return cpf.replace(/\D/g, '');
+}
+
 // Changes the date to display to users
-function formatesDateExibition (data) {
-  let date = new Date(data);
-  return date.toLocaleDateString('pt-BR');
+function formatesDateExibition(data) {
+  const [year, month, day] = data.split('-');
+  const date = new Date(year, month - 1, day);
+  return date.toLocaleDateString("pt-BR");
+}
+
+// Back to homepage
+function backToHome() {
+  window.location.href = "/web/index.html";
+}
+
+// Converts the state abbreviation to name
+function convertStateName(acronym) {
+
+  var data;
+
+  switch (acronym.toUpperCase()) {
+    case "AC" :
+      data = "Acre";
+      break;
+    case "AL" :
+      data = "Alagoas";
+      break;
+    case "AM" :
+      data = "Amazonas";
+      break;
+    case "AP" :
+      data = "Amapá";
+      break;
+    case "BA" :
+      data = "Bahia";
+      break;
+    case "CE" :
+      data = "Ceará";
+      break;
+    case "DF" :
+      data = "Distrito Federal";
+      break;
+    case "ES" :
+      data = "Espírito Santo";
+      break;
+    case "GO" :
+      data = "Goiás";
+      break;
+    case "MA" :
+      data = "Maranhão";
+      break;
+    case "MG" :
+      data = "Minas Gerais";
+      break;
+    case "MS" :
+      data = "Mato Grosso do Sul";
+      break;
+    case "MT" :
+      data = "Mato Grosso";
+      break;
+    case "PA" :
+      data = "Pará";
+      break;
+    case "PB" :
+      data = "Paraíba";
+      break;
+    case "PE" :
+      data = "Pernambuco";
+      break;
+    case "PI" :
+      data = "Piauí";
+      break;
+    case "PR" :
+      data = "Paraná";
+      break;
+    case "RJ" :
+      data = "Rio de Janeiro";
+      break;
+    case "RN" :
+      data = "Rio Grande do Norte";
+      break;
+    case "RO" :
+      data = "Rondônia";
+      break;
+    case "RR" :
+      data = "Roraima";
+      break;
+    case "RS" :
+      data = "Rio Grande do Sul";
+      break;
+    case "SC" :
+      data = "Santa Catarina";
+      break;
+    case "SE" :
+      data = "Sergipe";
+      break;
+    case "SP" :
+      data = "São Paulo";
+      break;
+    case "TO" :
+      data = "Tocantins";
+      break;
+  }
+
+  return data;
+};
+
+// Fetches CEP data
+function getDataCEP(cep, rua, bairro, cidade, estado) {
+
+  $.getJSON("https://viacep.com.br/ws/" + $("#" + cep).val().replace(/[^\d]+/g, "") + "/json/", function(data) {
+
+    var resultCEP = data;
+
+    if ((resultCEP["logradouro"] !== "" && resultCEP["logradouro"] !== undefined) || (resultCEP["localidade"] !== "" && resultCEP["localidade"] !== undefined)) {
+
+      $("#" + rua).val(unescape(resultCEP["logradouro"]));
+      if (unescape(resultCEP["logradouro"]) !== "") {
+        $("#" + rua).attr("readonly", true);
+      }
+
+      $("#" + bairro).val(unescape(resultCEP["bairro"]));
+      if (unescape(resultCEP["bairro"]) !== "") {
+        $("#" + bairro).attr("readonly", true);
+      }
+
+      $("#" + cidade).val(unescape(resultCEP["localidade"]));
+      if (unescape(resultCEP["localidade"]) !== "") {
+        $("#" + cidade).attr("readonly", true);
+      }
+
+      $("#" + estado).val(convertStateName(unescape(resultCEP["uf"])));
+      if (unescape(resultCEP["uf"]) !== "") {
+        $("#" + estado).attr("readonly", true);
+      }
+
+      $(".address").removeClass("hidden").fadeIn("slow");
+
+    } else if (resultCEP.erro) {
+      swal("", "Algo de errado! CEP não encontrado.", "warning");
+    }
+  });
 }
--- web/js/pacientes.js
+++ web/js/pacientes.js
@@ -4,7 +4,7 @@ function fetchesPatients () {
 
   $.get(`http://localhost:3000/pacientes/`, function (response) {
 
-    console.log(response);
+    // console.log(response);
 
     var table_body = 'table-body-patients';
 
@@ -23,12 +23,143 @@ function fetchesPatients () {
             '<td>'+ json['nome'] +'</td>' +
             '<td>'+ formatesCPF(json['cpf']) +'</td>' +
             '<td>'+ formatesDateExibition(json['data_nascimento']) +'</td>' +
+            '<td>' +
+              '<i class="fa-solid fa-trash-can me-3 icon-edit" title="Excluir" onclick="deletePacient (' + json['id'] + ')"></i>' +
+              '<i class="fa-solid fa-pen-to-square icon-delete" title="Editar" data-bs-toggle="modal" data-bs-target="#updatePatientModal" onclick="fetchesPatientById (' + json['id'] + ')"></i>' +
+            '</td>' +
             '</tr>'
           );
       });
     } else {
       $('#table-body-patients')
         .html('<tr class="text-center"><td colspan="7">Nenhum registro encontrado!</td></tr>');
+    }
+  });
+}
+
+// Register new pacient
+function registerPacient (data) {
+
+  $.ajax({
+    url: 'http://localhost:3000/pacientes',
+    dataType: 'json',
+    type: 'post',
+    data: data,
+
+    success: function (response) {
+
+      toastr.success('Paciente cadastrado com sucesso!', '', {
+        closeButton: true,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        preventDuplicates: true,
+        showDuration: "500",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut"
+      });
+
+      setTimeout(function () {
+        backToHome();
+      }, 2000);
+    }
+  });
+}
+
+// Deletes patient
+function deletePacient (id) {
+
+  $.ajax({
+    url: 'http://localhost:3000/pacientes/' + id,
+    dataType: 'json',
+    type: 'delete',
+
+    success: function (response) {
+
+      toastr.success('Paciente excluído com sucesso!', '', {
+        closeButton: true,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        preventDuplicates: true,
+        showDuration: "500",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut"
+      });
+
+      setTimeout(function () {
+        location.reload();
+      }, 2000);
+    }
+  });
+}
+
+// Fetches register by id
+function fetchesPatientById (id) {
+
+  $.ajax({
+    url: 'http://localhost:3000/pacientes/' + id,
+    dataType: 'json',
+    type: 'get',
+
+    success: function (response) {
+      // console.log(response);
+
+      var data = response.data;
+
+      if (data) {
+        $('#id').val(data.id);
+        $('#nome').val(data.nome);
+        $('#cpf').val(data.cpf).addClass('cpf');
+        $('#data_nascimento').val(data.data_nascimento)
+        $('#email').val(data.email)
+        $('#telefone').val(data.telefone)
+        $('#cep').val(data.cep).addClass('cep');
+        $('#rua').val(data.logradouro);
+        $('#bairro').val(data.bairro);
+        $('#cidade').val(data.localidade);
+        $('#estado').val(data.uf);
+      }
+    },
+
+    error: function (xhr, status, error) {
+      toastr.error(error, '', {
+        closeButton: true,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        preventDuplicates: true,
+        showDuration: "500",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut"
+      });
+    }
+  });
+}
+
+// Updates patient data
+function updatePacient (id, data) {
+
+  $.ajax({
+    url: 'http://localhost:3000/pacientes/' + id,
+    dataType: 'json',
+    type: 'patch',
+    data: data,
+
+    success: function (response) {
+
+      toastr.success('Paciente atualizado com sucesso!', '', {
+        closeButton: true,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        preventDuplicates: true,
+        showDuration: "1000",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut"
+      });
+
+      $('#updatePatientModal').modal('hide');
+
+      setTimeout(function () {
+        location.reload();
+      }, 3000);
     }
   });
 }
